36ebfe2f74cf0b9195f5f6f07f69cb6e4a3c513d
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2024-07-01 22:37:53 +0700

    Setup colors and shadows

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import { type NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <>
-      <Heading>Hello!</Heading>
+      <Heading shadow="brand.double">Hello!</Heading>
       <Text>Meow</Text>
     </>
   );
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -5,5 +5,26 @@ export const theme = extendTheme({
   fonts: {
     heading: "var(--font-presstart2p)",
     body: "var(--font-montserrat)"
+  },
+  colors: {
+    brand: {
+      sage: {
+        0: "hsla(146, 20%, 63.7%, 1)"
+      },
+      charcoal: {
+        0: "hsla(0, 0%, 10.6%, 1)"
+      },
+      white: {
+        0: "hsla(30, 57.1%, 97.3%, 1)"
+      },
+      orange: {
+        0: "hsla(13, 87.9%, 54.5%, 1)"
+      }
+    }
+  },
+  shadows: {
+    brand: {
+      double: "8px 8px 0px rgba(144, 181, 160, 1), 4px 4px 0px rgba(27, 27, 27, 1)"
+    }
   }
 });
