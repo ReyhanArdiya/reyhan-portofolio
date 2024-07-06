f693599040975df241b532113c64f5e708c98819
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2024-07-06 22:44:25 +0700

    Add down arrow

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,18 @@
 import { Heading, Text } from "@chakra-ui/react";
 import { type NextPage } from "next";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
     <>
-      <Heading shadow="brand.double">Hello!</Heading>
+      <Heading>Hello!</Heading>
       <Text>Meow</Text>
+      <Image
+        src="/assets/arrow_down_black.png"
+        alt="Black down arrow"
+        width={45}
+        height={50}
+      />
     </>
   );
 };
