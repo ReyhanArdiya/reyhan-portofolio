3e9c7a6fdbc2f878b4a2b359452ef7d928118ab1
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2024-07-08 17:05:14 +0700

    Create `StorySection`

---
+++ src/components/StorySection/index.tsx
@@ -0,0 +1,75 @@
+import { Heading, StackProps, Text, VStack } from "@chakra-ui/react";
+import { useTranslation } from "next-i18next";
+import { ReactNode } from "react";
+
+const highlightTitle = (title: string) => {
+  const titleSplit = title.split(/(<%.*%>)/gm);
+
+  return titleSplit.map((t, i) => {
+    if (t.match(/<.*>/)) {
+      return (
+        <Text as="span" key={i} color="brand.sage.0">
+          {t.replace(/<%|%>/g, "")}
+        </Text>
+      );
+    } else {
+      return (
+        <Text as="span" color="inherit" key={i}>
+          {t}
+        </Text>
+      );
+    }
+  });
+};
+
+export interface StorySectionProps {
+  // title: string;
+  image: ReactNode;
+  // paragraphs: string[];
+  bg?: StackProps["bg"];
+  textColor?: StackProps["color"];
+  storyIndex: number;
+}
+
+const StorySection = ({
+  storyIndex,
+  image,
+  bg = "brand.white.0",
+  textColor = "brand.charcoal.0"
+}: StorySectionProps) => {
+  const { t } = useTranslation("stories");
+
+  const title = t(`${storyIndex}.title`) as string;
+  const paragraphs = t(`${storyIndex}.paragraphs`, {
+    returnObjects: true
+  }) as string[];
+
+  const pTexts = paragraphs.map((p, i) => (
+    <Text color="inherit" textAlign="center" key={i} fontSize="md">
+      {p}
+    </Text>
+  ));
+
+  const titleText = highlightTitle(title);
+
+  return (
+    <VStack
+      color={textColor}
+      spacing="6"
+      justify="center"
+      minH="100vh"
+      bg={bg}
+      w="full"
+      px="4"
+      py="3"
+    >
+      <Heading color="inherit" textAlign="center" fontSize="x-large" as="h2">
+        {titleText}
+      </Heading>
+      {image}
+      {pTexts}
+    </VStack>
+  );
+};
+
+export default StorySection;
