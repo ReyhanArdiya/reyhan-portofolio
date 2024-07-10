3d6253fb58a245c4fe36fa9042f6d9f6a62fb0c3
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2024-07-10 16:40:18 +0700

    Fetch projects from locales

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -5,6 +5,8 @@ import { VStack } from "@chakra-ui/react";
 import { collection, CollectionReference, getDocs } from "firebase/firestore";
 import { GetStaticProps, type NextPage } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
+import { createInstance } from "i18next";
 
 export interface ProjectsPageProps {
   projects: Project[];
@@ -13,23 +15,32 @@ export interface ProjectsPageProps {
 export const getStaticProps: GetStaticProps<ProjectsPageProps> = async ({
   locale
 }) => {
-  const db = serverDb;
+  // const db = serverDb;
 
-  const projectsCollection = collection(
-    db,
-    "projects"
-  ) as CollectionReference<Project>;
+  // const projectsCollection = collection(
+  //   db,
+  //   "projects"
+  // ) as CollectionReference<Project>;
 
-  const projectsSnapshot = await getDocs(projectsCollection);
+  // const projectsSnapshot = await getDocs(projectsCollection);
 
-  const projects = projectsSnapshot.docs.map(doc => ({
-    id: doc.id,
-    ...doc.data()
-  })) as Project[];
+  // const projects = projectsSnapshot.docs.map(doc => ({
+  //   id: doc.id,
+  //   ...doc.data()
+  // })) as Project[];
+
+  const tr = await serverSideTranslations(locale!, ["common", "navbar", "projects"]);
+  const i18n = createInstance({
+    lng: tr?._nextI18Next?.initialLocale,
+    ns: tr?._nextI18Next?.ns,
+    resources: tr?._nextI18Next?.initialI18nStore
+  });
+  i18n.init();
+  const projects = i18n.t("projects:projects", { returnObjects: true }) as Project[];
 
   return {
     props: {
-      ...(await serverSideTranslations(locale!, ["common", "navbar", "projects"])),
+      ...tr,
       projects
     }
   };
